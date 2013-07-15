2fea9c8bf27c93b986cd832ff6d074cb305d5b77
jon <xuchaoo@gmail.com>
2013-07-15 21:04:48 +0800

    solr 删除和使用xml做index的脚本

---
+++ solr-cmd/delete.cmd
@@ -0,0 +1,12 @@
+java -Ddata=args -Durl=http://www.myhack.org:8080/solr/shard_2/update -jar post.jar "<delete><id>51a51f4d89fa8</id></delete>"
+java -Ddata=args -Durl=http://www.myhack.org:8080/solr/shard_2/update -jar post.jar "<delete><id>51a42da9c4bf0</id></delete>"
+
+java -Ddata=args -Durl=http://www.myhack.org:8080/solr/shard_2/update -jar post.jar "<delete><id>51a4cc4e7bf06</id></delete>"
+
+java -Ddata=args -Durl=http://www.myhack.org:8080/solr/shard_2/update -jar post.jar "<delete><id>51a42e80e4a8d</id></delete>"
+java -Ddata=args -Durl=http://www.myhack.org:8080/solr/shard_2/update -jar post.jar "<delete><id>51a41d28e3839</id></delete>"
+java -Ddata=args -Durl=http://www.myhack.org:8080/solr/shard_2/update -jar post.jar "<delete><id>51a52128c3176</id></delete>"
+java -Ddata=args -Durl=http://www.myhack.org:8080/solr/shard_2/update -jar post.jar "<delete><id>51a51f4d87f19</id></delete>"
+
+
+pause;
---
+++ solr-cmd/postxml.cmd
@@ -0,0 +1,2 @@
+java -Durl=http://localhost:8983/solr/core_name/update/ -jar post.jar doc.xml
+pause
